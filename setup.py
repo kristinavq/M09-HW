1c2e641e5b17310bca57bd5c2a71673c5977b93f
Kristina_Vanessa_Quintana <you@example.com>
2025-03-23 17:31:49 -0400

    Add changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='booklover',
     version='0.1',
     description= 'Package booklover.py',
-    url='http://github.com/kvq4nvn/M09-HW'
+    url='http://github.com/kvq4nvn/M09-HW',
     author='kristinavq',
     license='MIT',
     packages=['booklover'],
